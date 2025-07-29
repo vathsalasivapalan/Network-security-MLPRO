f4989519dde8b481516dd247bb43d8a6f431577d
vathsalasivapalan <vathsalasivapalan@gmail.com>
2025-07-29 20:48:57 +0200

    get data

--- get_data.py
+++ get_data.py
@@ -10,4 +10,30 @@ import pandas as pd
 import numpy as np
 import pymongo 
 
-from net
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger  import logging
+
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    def csv_to_json_convertor(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    def pushing_data_to_mangodb(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    if __name__ == '__main__':
+        pass 
